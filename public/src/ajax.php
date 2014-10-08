2a60cfd823aa4c2e21d83d6ef49b8ac710953a54
Roman Ožana <roman@wikidi.com>
2014-10-08 16:06:05 +0200

    Fix ajax

--- public/src/ajax.php
+++ public/src/ajax.php
@@ -4,7 +4,7 @@ namespace vestibulum;
 isset($this) && $this instanceof Vestibulum or die('Sorry can be executed only from Vestibulum');
 
 // check AJAX request
-isAjax() or status(500) and nocache(json(['message' => 'Not AJAX request, but nice try :-)']));
+isAjax() or status(500) and die(json(['message' => 'Not AJAX request, but nice try :-)']));
 
 // response all AJAX requests
-json(['message' => 'Well done! It\'s AJAX request']);
+nocache(json(['message' => 'Well done! It\'s AJAX request']));
