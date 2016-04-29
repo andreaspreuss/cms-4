09e88a94a01ecaba7da84bb38140962e4d06e611
Roman Ožana <roman@wikidi.com>
2016-04-29 12:21:07 +0200

    Upgrade config

--- public/config.php
+++ public/config.php
@@ -3,7 +3,9 @@
 return [
 	'title' => 'Sphido CMS',
 	'cache' => realpath(__DIR__ . '/../cache/'),
+	'content' => realpath(__DIR__ . '/pages'),
 	'meta' => [
-		'author' => 'All: Roman Ozana; e-mail: ozana@omdesign.cz'
+		'author' => 'All: Roman Ozana; e-mail: ozana@omdesign.cz',
+		'template' => realpath(__DIR__ . '/layout.latte')
 	]
 ];
